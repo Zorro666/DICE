a8baaafb4327b7f2a97d87489f5c01a2daa32e18
Jake <jake@evansturner.co.uk>
2013-01-20 07:13:24 +0000

    added combinations and counting

--- dice.c
+++ dice.c
@@ -9,14 +9,36 @@ static int rollDice(void)
 	return dice;
 }
 
-static int s_rolls[13];
+#define MIN_DICE_ROLL (2)
+#define MAX_DICE_ROLL (12)
+#define MAX_NUM_COMBINATIONS (1024)
+
+typedef struct Combination
+{
+	int m_roll[3];
+	int m_count;
+	int m_delta;
+} Combination;
+
+static int s_rolls[MAX_DICE_ROLL+1];
 static int s_nRolls;
+static int s_numCombinations;
+static Combination s_combinations[MAX_NUM_COMBINATIONS];
+
+static void combinationInit(Combination* const pThis)
+{
+	pThis->m_roll[0] = 0;
+	pThis->m_roll[1] = 0;
+	pThis->m_roll[2] = 0;
+	pThis->m_count = 0;
+	pThis->m_delta = 0;
+}
 
 static void printRolls(void)
 {
 	int i;
-	printf("%d\n", s_nRolls);
-	for (i = 2; i <= 12; i++)
+	printf("numRolls = %d\n", s_nRolls);
+	for (i = MIN_DICE_ROLL; i <= MAX_DICE_ROLL; i++)
 	{
 		const double num = (double)(s_rolls[i]);
 		const double total = (double)s_nRolls;
@@ -26,6 +48,129 @@ static void printRolls(void)
 	}
 }
 
+static void printCombinations(void)
+{
+	int i;
+	const int numCombinations = s_numCombinations;
+	for (i = 0; i < numCombinations; i++)
+	{
+		const Combination* const pCombination = &s_combinations[i];
+		if (pCombination->m_count != 0)
+		{
+			const double num = (double)(pCombination->m_count);
+			const double total = (double)s_nRolls;
+			const double ratio = num/total;
+			const float pc = (float)(100.0*ratio);
+			printf("%d, %d, %d = %3.1f\n", pCombination->m_roll[0], pCombination->m_roll[1], pCombination->m_roll[2], pc);
+		}
+	}
+}
+
+static int addCombination(const int a, const int b, const int c)
+{
+	int i;
+	const int numCombinations = s_numCombinations;
+	Combination* const pNewCombination = &s_combinations[numCombinations];
+	int rolls[3];
+
+	if ((a == b) || (a == c) || (b == c))
+	{
+		return 0;
+	}
+	rolls[0] = a;
+	rolls[1] = b;
+	rolls[2] = c;
+
+	for (i = 0; i < 2; i++)
+	{
+		int j;
+		for (j = i+1; j < 3; j++)
+		{
+			const int rollI = rolls[i];
+			const int rollJ = rolls[j];
+			if (rollJ < rollI)
+			{
+				const int temp = rollI;
+				rolls[i] = rollJ;
+				rolls[j] = temp;
+			}
+		}
+	}
+
+	for (i = 0; i < numCombinations; i++)
+	{
+		const Combination* const pCombination = &s_combinations[i];
+		int roll;
+		int match = 1;
+		for (roll = 0; roll < 3; roll++)
+		{
+			if (pCombination->m_roll[roll] != rolls[roll])
+			{
+				match = 0;
+				break;
+			}
+		}
+		if (match == 1)
+		{
+			return 0;
+		}
+	}
+	pNewCombination->m_roll[0] = rolls[0];
+	pNewCombination->m_roll[1] = rolls[1];
+	pNewCombination->m_roll[2] = rolls[2];
+	pNewCombination->m_count = 0;
+	pNewCombination->m_delta = 0;
+	s_numCombinations++;
+
+	return 1;
+}
+
+static void addRollToCombinations(const int roll)
+{
+	const int numCombinations = s_numCombinations;
+	int i;
+	for (i = 0; i < numCombinations; i++)
+	{
+		const Combination* const pCombination = &s_combinations[i];
+		const int* const pRolls = pCombination->m_roll;
+		if ((pRolls[0] == roll) || (pRolls[1] == roll) || (pRolls[2] == roll))
+		{
+			s_combinations[i].m_delta = 1;
+		}
+	}
+}
+
+static void updateCombinations(void)
+{
+	const int numCombinations = s_numCombinations;
+	int i;
+	for (i = 0; i < numCombinations; i++)
+	{
+		Combination* const pCombination = &s_combinations[i];
+		pCombination->m_count += pCombination->m_delta;
+		pCombination->m_delta = 0;
+	}
+}
+
+static int makeCombinations(void)
+{
+	int numComb = 0;
+	int i;
+	for (i = MIN_DICE_ROLL; i <= MAX_DICE_ROLL-2; i++)
+	{
+		int j;
+		for (j = i+1; j <= MAX_DICE_ROLL-1; j++)
+		{
+			int k;
+			for (k = j+1; k <= MAX_DICE_ROLL; k++)
+			{
+				numComb += addCombination(i, j, k);
+			}
+		}
+	}
+	return numComb;
+}
+
 int main(int argc, char* argv[])
 {
 	int i;
@@ -33,11 +178,20 @@ int main(int argc, char* argv[])
 
 	printf("argc %d argv[0] '%s'\n", argc, argv[0]);
 
-	for (i = 0; i < 13; i++)
+	for (i = 0; i < MAX_DICE_ROLL+1; i++)
 	{
 		s_rolls[i] = 0;
 	}
 	s_nRolls = 0;
+
+	for (i = 0; i < MAX_NUM_COMBINATIONS; i++)
+	{
+		combinationInit(&s_combinations[i]);
+	}
+	s_numCombinations = 0;
+	s_numCombinations = makeCombinations();
+	printf("numCombinations %d\n", s_numCombinations);
+	printCombinations();
 
 	count = 0;
 	do
@@ -87,14 +241,18 @@ int main(int argc, char* argv[])
 		for (i = 0; i < placeToAdd; i++)
 		{
 			const int roll = rollsToAdd[i];
+			addRollToCombinations(roll);
 			s_rolls[roll]++;
 		}
 
+		updateCombinations();
 		s_nRolls++;
 
 		if ((count % (1024*1024)) == 0)
 		{
 			printRolls();
+			printCombinations();
+			printf("numRolls %d\n", s_nRolls);
 		}
 		count++;
 	} while (count < 1*1024*1024*1024);
